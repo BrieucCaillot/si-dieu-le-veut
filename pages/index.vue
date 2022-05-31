b28f94ffbf3f4ba27cf7711fb6b27f2f250f2c21
Brieuc Caillot <brieucdeveloper@gmail.com>
2022-05-31 14:10:17 +0200

    Added typing inside component

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,7 @@
 <template>
-  <div>
-    <OrdalieCroix v-if="showOrdalie(ORDALIES.CROIX)" />
-    <OrdalieBBQ v-if="showOrdalie(ORDALIES.BBQ)" />
-    <OrdalieFood v-if="showOrdalie(ORDALIES.FOOD)" />
-    <OrdalieCauldron v-if="showOrdalie(ORDALIES.CAULDRON)" />
-  </div>
+  <Typing />
 </template>
 
 <script setup lang="ts">
-import OrdalieCroix from '@/components/ui/Typing/OrdalieCroix.vue'
-import OrdalieBBQ from '@/components/ui/Typing/OrdalieBBQ.vue'
-import OrdalieFood from '@/components/ui/Typing/OrdalieFood.vue'
-import OrdalieCauldron from '@/components/ui/Typing/OrdalieCauldron.vue'
-import ORDALIES from '@/constants/ORDALIES'
-
-const currentOrdalie = ref(null)
-
-onMounted(() => (currentOrdalie.value = useStore().currentOrdalie.value))
-
-watch(useStore().currentOrdalie, (value: ORDALIES) => (currentOrdalie.value = value))
-
-const showOrdalie = (_ordalie: ORDALIES) => currentOrdalie.value === _ordalie
+import Typing from '@/components/ui/Typing/Typing.vue'
 </script>
