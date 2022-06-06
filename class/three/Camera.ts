8a8f8cddfe954bcf29e56f1475fce86a8787ad95
Brieuc Caillot <bcaillot@atomicdigital.design>
2022-06-06 15:15:44 +0200

    Removed useless code in Camera

--- class/three/Camera.ts
+++ class/three/Camera.ts
@@ -68,61 +68,6 @@ class Camera extends THREE.EventDispatcher {
     if (WebGL.debug.isActive()) this.debugFolder.add(this.controls, 'enabled')
   }
 
-  getXFOV() {
-    // Convert angle to radiant
-    const FOV = this.instance.fov
-    let yFovRadiant = (FOV * Math.PI) / 180
-    // Calculate X-FOV Radiant
-    let xFovRadiant = 2 * Math.atan(Math.tan(yFovRadiant / 2) * (window.innerWidth / window.innerHeight))
-    // Convert back to angle
-    let xFovAngle = (xFovRadiant * 180) / Math.PI
-    return xFovAngle
-  }
-
-  /**
-   * Convert vertical field of view to horizontal field of view, given an aspect
-   * ratio. See https://arstechnica.com/civis/viewtopic.php?f=6&t=37447
-   *
-   * @param vfov - The vertical field of view.
-   * @param aspect - The aspect ratio, which is generally width/height of the viewport.
-   * @returns - The horizontal field of view.
-   */
-  vfovToHfov(vfov: number, aspect: number): number {
-    const { tan, atan } = Math
-    return atan(aspect * tan(vfov / 2)) * 2
-  }
-
-  /**
-   * Get the distance from the camera to fit an object in view by either its
-   * horizontal or its vertical dimension.
-   *
-   * @param size - This should be the width or height of the object to fit.
-   * @param fov - If `size` is the object's width, `fov` should be the horizontal
-   * field of view of the view camera. If `size` is the object's height, then
-   * `fov` should be the view camera's vertical field of view.
-   * @returns - The distance from the camera so that the object will fit from
-   * edge to edge of the viewport.
-   */
-  _distanceToFitObjectInView(size: number, fov: number): number {
-    const { tan } = Math
-    return size / (2 * tan(fov / 2))
-  }
-
-  distanceToFitObjectToView(cameraAspect: number, cameraVFov: number, objWidth: number, objHeight: number): number {
-    const objAspect = objWidth / objHeight
-    const cameraHFov = this.vfovToHfov(cameraVFov, cameraAspect)
-
-    let distance: number = 0
-
-    if (objAspect > cameraAspect) {
-      distance = this._distanceToFitObjectInView(objHeight, cameraVFov)
-    } else if (objAspect <= cameraAspect) {
-      distance = this._distanceToFitObjectInView(objWidth, cameraHFov)
-    }
-
-    return distance
-  }
-
   private setFov() {
     let dist = this.parent.position.z - 0
     let height = 1 // desired height to fit
