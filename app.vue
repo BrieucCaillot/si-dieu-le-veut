fd959d492645f11fa7ec9ba0a61f77823cb673e6
Brieuc Caillot <brieucdeveloper@gmail.com>
2022-06-12 22:08:57 +0200

    Added Meta & OG Meta tags

--- app.vue
+++ app.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
-    <Head lang="en> 50 ? 'en-GB' : 'en-US'">
+    <Head lang="fr">
       <Title>Si Dieu le veut</Title>
+      <Meta name="keywords" content="si dieu le veut, project, threejs, nuxt3, vue, gobelins, 2022, brieuc caillot, karl justiniano, ouri levin, daria duvernois, emma rimbert" />
       <Meta name="description" content="Si Dieu le veut" />
+      <Meta name="og:title" content="Si Dieu le veut" />
+      <Meta name="og:description" content="Si Dieu le veut description" />
+      <Meta name="og:type" content="website" />
+      <Meta name="og:url" content="https://si-dieu-le-veut.vercel.app/" />
+      <Meta name="og:image" content="https://si-dieu-le-veut.vercel.app/images/preview.png" />
+      <Meta name="og:image:width" content="1200" />
+      <Meta name="og:image:height" content="630" />
+      <Meta name="og:image:type" content="image/png" />
+      <Meta name="og:image:alt" content="Si Dieu le veut" />
+      <Meta name="twitter:card" content="summary_large_image" />
+      <Meta name="twitter:title" content="Si Dieu le veut" />
+      <Meta name="twitter:description" content="Si Dieu le veut description" />
+      <Meta name="twitter:image" content="https://si-dieu-le-veut.vercel.app/images/preview.png" />
+      <Meta name="twitter:image:alt" content="Si Dieu le veut - Preview" />
     </Head>
     <NuxtLayout name="default">
       <NuxtPage />
