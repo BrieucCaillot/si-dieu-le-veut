a68a5d3961347e550869883ae3f45b2058312ce8
Brieuc Caillot <bcaillot@atomicdigital.design>
2022-06-14 16:16:08 +0200

    ✨ Instantiate random transition

--- class/three/World/Block.ts
+++ class/three/World/Block.ts
@@ -80,10 +80,11 @@ class Block {
    */
   private add() {
     const bg = this.model.scene.children.find((child) => child.name === 'background')
-    // const newMat = new THREE.MeshBasicMaterial({
-    //   color: 0xe6e1db,
-    // })
-    // bg.material = newMat
+    const newMat = new THREE.MeshBasicMaterial({
+      name: 'background',
+      color: 0xe6e1db,
+    })
+    bg.material = newMat
     this.size = new THREE.Box3().setFromObject(bg).getSize(new THREE.Vector3())
     WebGL.scene.add(this.model.scene)
   }
