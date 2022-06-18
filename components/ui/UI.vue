284a7bf422577a81d0755480ca594771e4e279d3
Brieuc Caillot <bcaillot@atomicdigital.design>
2022-06-18 12:30:13 +0200

    ⚰️ Removed unused video component

--- components/ui/UI.vue
+++ components/ui/UI.vue
@@ -4,7 +4,6 @@
   <main id="main" class="page">
     <template v-if="startWebGL">
       <Loader v-if="showLoader" />
-      <Video />
       <Canvas />
       <Typing />
       <Score v-if="isEndOrDead()" />
@@ -19,14 +18,12 @@
 import Canvas from '@/components/three/Canvas.vue'
 
 import Loader from '@/components/ui/Loader.vue'
-import Video from '@/components/ui/Video.vue'
 import HUD from '@/components/ui/HUD.vue'
 import Mobile from '@/components/ui/Mobile.vue'
 import Typing from '@/components/ui/Typing/Typing.vue'
 import Score from '@/components/ui/Score.vue'
 
 import { isMobileTest } from '@/class/three/utils/isMobile'
-import Blocks from '@/class/three/World/Blocks'
 import ORDALIES from '@/constants/ORDALIES'
 import OTHERS from '@/constants/OTHERS'
 import TRANSITIONS from '@/constants/TRANSITIONS'
